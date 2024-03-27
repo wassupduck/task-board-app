38adf793d160d6de633773ff4710919c4ba113c5
Jono Brogan <jonobrogan123@gmail.com>
2024-03-27 12:32:26 +0000

    fix filename case

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./Global.module.css";
+import "./global.module.css";
 
 import App from "./components/App/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
